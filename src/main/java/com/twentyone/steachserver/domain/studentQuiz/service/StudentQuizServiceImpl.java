bccefe8704a0685b2aedabd7018a7416184ca980
Juhyorim <joohr1234@gmail.com>
2025-01-27 00:55:18 +0900

    refactor: StudentQuiz 생성 시 통계정보 생성에 실패해도 비즈니스 로직이 실패하지 않도록 예외처리 로직 추가
    related-to: #200

--- src/main/java/com/twentyone/steachserver/domain/studentQuiz/service/StudentQuizServiceImpl.java
+++ src/main/java/com/twentyone/steachserver/domain/studentQuiz/service/StudentQuizServiceImpl.java
@@ -2,7 +2,6 @@ package com.twentyone.steachserver.domain.studentQuiz.service;
 
 import com.twentyone.steachserver.domain.lecture.validator.LectureValidator;
 import com.twentyone.steachserver.domain.member.model.Student;
-import com.twentyone.steachserver.domain.member.repository.StudentRepository;
 import com.twentyone.steachserver.domain.quiz.model.Quiz;
 import com.twentyone.steachserver.domain.quiz.model.QuizStatistics;
 import com.twentyone.steachserver.domain.quiz.repository.QuizRepository;
@@ -22,36 +21,46 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StudentQuizServiceImpl implements StudentQuizService {
-
     private final StudentQuizRepository studentQuizzesRepository;
-    private final StudentRepository studentRepository;
     private final QuizRepository quizRepository;
     private final QuizStatisticsRepository quizStatisticsRepository;
 
-    private final LectureValidator lectureValidator;
-
     @Secured("ROLE_STUDENT")
     @Transactional
-    public StudentQuiz createStudentQuiz(Student student, Integer quizId, StudentQuizRequestDto requestDto){
+    public StudentQuiz createStudentQuiz(Student student, Integer quizId, StudentQuizRequestDto requestDto) {
         Quiz quiz = quizRepository.findById(quizId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 퀴즈입니다."));
 
         //lectureValidator.validateFinishLecture(quiz.getLecture());
         //lectureValidator.validateQuizOfLectureAuth(quiz, student);
 
-        Optional<StudentQuiz> studentQuiz = studentQuizzesRepository.findById(StudentQuizId.createStudentQuizId(student.getId(), quizId));
-        if (studentQuiz.isPresent()) throw new IllegalArgumentException("이미 점수가 존재합니다.");
+        Optional<StudentQuiz> studentQuiz = studentQuizzesRepository.findById(
+                StudentQuizId.createStudentQuizId(student.getId(), quizId));
+        if (studentQuiz.isPresent()) {
+            throw new IllegalArgumentException("이미 점수가 존재합니다.");
+        }
 
         StudentQuiz newStudentQuiz = StudentQuiz.createStudentQuiz(student, quiz, requestDto);
         studentQuizzesRepository.save(newStudentQuiz);
 
-        //통계생성 - 나중에 Redis로 바꾸지
-        QuizStatistics quizStatistics = quizStatisticsRepository.findByStudentIdAndLectureId(student.getId(), quiz.getLecture().getId())
-                .orElseGet(() -> new QuizStatistics(newStudentQuiz.getQuiz().getLecture().getId(), newStudentQuiz.getStudent().getId()));
+        //통계생성 - 실패해도 계속 진행하도록 처리
+        try {
+            createStatistics(student, requestDto, quiz, newStudentQuiz);
+        } catch (RuntimeException e) {
+            //pass
+        }
+
+        return newStudentQuiz;
+    }
+
+    private void createStatistics(Student student, StudentQuizRequestDto requestDto, Quiz quiz,
+                                  StudentQuiz newStudentQuiz) {
+        QuizStatistics quizStatistics = quizStatisticsRepository.findByStudentIdAndLectureId(
+                        student.getId(), quiz.getLecture().getId())
+                .orElseGet(() -> new QuizStatistics(
+                        newStudentQuiz.getQuiz().getLecture().getId(), newStudentQuiz.getStudent().getId()));
         quizStatistics.update(requestDto.score());
 
         quizStatisticsRepository.save(quizStatistics);
-
-        return newStudentQuiz;
     }
 }
